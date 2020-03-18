4309e8f116c9d05be244b681af1b2e743508d34d
Manpreet <lvlanu29@gmail.com>
2020-03-17 20:54:44 -0400

    Initial Implementation

---
+++ reader.py
@@ -0,0 +1,89 @@
+import string
+
+
+class Reader:
+    def __init__(self, file, v, n):
+        self.file = file
+        self.v = v
+        self.n = n
+        with open(file, 'r', encoding='UTF8') as f:
+            self.content = f.readlines()
+        f.close()
+        # In case to read it top to bottom
+        # self.content.reverse()
+
+    def has_next(self):
+        return False if self.content == [] else True
+
+    def next(self):
+        if self.has_next() is False:
+            return None
+
+        # Grab one line and split it onto list
+        line = self.content.pop()
+        line = line.split()
+        line.reverse()
+
+        # Segregate id, username and language
+        id = line.pop()
+        user = line.pop()
+        language = line.pop()
+
+        # Remove any terms starting with #, @ or http
+        for element in line:
+            if element.startswith("#") or element.startswith("@") or element.startswith("http"):
+                line.remove(element)
+
+        if self.v == 0 or self.v == 1:
+            if self.v == 0:
+                vocabulary = list(string.ascii_lowercase)
+                for element in line:
+                    element.lower()
+
+            elif self.v == 1:
+                vocabulary = list(string.ascii_letters)
+
+            # Replace any character not within vocabulary as space, potentially splitting word and add it back into list
+            new_line = []
+            for index in range(len(line)):
+                characters = list(line[index])
+                for character in characters:
+                    if character not in vocabulary:
+                        line[index] = line[index].replace(character, " ")
+                new_line += line[index].split()
+
+        else:
+            if self.v == 0:
+                vocabulary = list(string.ascii_lowercase)
+                for element in line:
+                    element.lower()
+
+            elif self.v == 1:
+                vocabulary = list(string.ascii_letters)
+
+            # Replace any character not within vocabulary as space, potentially splitting word and add it back into list
+            # See example in section 1.2.2
+            new_line = []
+            for index in range(len(line)):
+                if line[index].isalpha():
+                    characters = list(line[index])
+                    for character in characters:
+                        if character not in vocabulary:
+                            line[index] = line[index].replace(character, " ")
+                new_line += line[index].split()
+
+        grams = []
+        for term in new_line:
+            index = 0;
+            while index + self.n <= len(term):
+                grams.append(term[index:index+self.n])
+                index += 1
+
+        return grams
+
+
+sample = Reader("training-tweets.txt", 1, 3)
+print(sample.next())
+print(sample.next())
+print(sample.next())
+
