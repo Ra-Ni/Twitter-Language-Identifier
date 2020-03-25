5fc737b3f3ae60daf76c6f40243f7bc7b7cb1a00
Manpreet <lvlanu29@gmail.com>
2020-03-25 11:38:00 -0400

    Implemented custom voc

---
+++ twitter/vocabularies/custom.py
@@ -0,0 +1,36 @@
+from re import compile
+import string
+
+from vocabularies import Vocabulary
+
+
+class Custom(Vocabulary):
+
+    def __init__(self, window_size=1):
+        super().__init__(window_size)
+        languages = {
+            "basque" : list('Ññ'),
+            "catalan" : list('ÇçàÀèÈéÉíÍïÏòÒóÓúÚüÜ'),
+            "galician" : list('ÑñáÁéÉïÏíÍóÓüÜ'),
+            "spanish" : list('ÁÉÍÓÚÜÑáéíóúüñ¿¡'),
+            "portuguese" : list('áÁâÂãÃàÀçÇéÉêÊíÍóÓôÔõÕúÚ'),
+        }
+
+        self.__pattern = dict.fromkeys(list(string.ascii_letters), 1)
+        for key in languages:
+            print(languages[key])
+            for char in languages[key]:
+                if self.__pattern.get(char) is None:
+                    self.__pattern[char] = 1
+
+    def accepts(self, item) -> bool:
+        if self.__patern[item] is 1:
+            return True
+        else:
+            return False
+
+    def __len__(self) -> int:
+        return len(self.__patern)
+
+    def __str__(self):
+        return '0'
